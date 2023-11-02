1bc58aed6af269ada8569ea64de0b15da529eafb
SDZZGNDRC <705258871@qq.com>
2023-11-02 20:21:47 +0800

    ch06

--- code-ch06/op.py
+++ code-ch06/op.py
@@ -645,9 +645,13 @@ def op_sha256(stack):
 
 def op_hash160(stack):
     # check that there's at least 1 element on the stack
+    if len(stack) < 1:
+        return False
     # pop off the top element from the stack
+    element = stack.pop()
     # push a hash160 of the popped off element to the stack
-    raise NotImplementedError
+    stack.append(hash160(element))
+    return True
 
 
 # tag::source2[]
@@ -662,13 +666,24 @@ def op_hash256(stack):
 
 def op_checksig(stack, z):
     # check that there are at least 2 elements on the stack
+    if len(stack) < 2:
+        return False
     # the top element of the stack is the SEC pubkey
+    pubkey_b = stack.pop()
     # the next element of the stack is the DER signature
+    sig_b = stack.pop()
     # take off the last byte of the signature as that's the hash_type
+    sig_b = sig_b[:-1]
     # parse the serialized pubkey and signature into objects
+    pubkey = S256Point.parse(pubkey_b)
+    sig = Signature.parse(sig_b)
     # verify the signature using S256Point.verify()
     # push an encoded 1 or 0 depending on whether the signature verified
-    raise NotImplementedError
+    if pubkey.verify(z, sig):
+        stack.append(b'\x01')
+    else:
+        stack.append(b'')
+    return True
 
 
 def op_checksigverify(stack, z):
